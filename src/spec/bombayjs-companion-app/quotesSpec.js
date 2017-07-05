51535b5a4f9c7b87d530493b8133a9dc4c194926
himansudesai <himansudesai@gmail.com>
2017-07-04 17:08:09 -0700

    Temporarily removing the 'VISIT' command from the Jasmine spec (for the bombayjs-companion-app sample app) because we've removed the 'VISIT' command capability from bombay.js client.  This is the ability to visit random websites(URLs in browser) which needs a bit more thinking.  This commit does not contain any changes to bombay.js framework itself.

--- src/spec/bombayjs-companion-app/quotesSpec.js
+++ src/spec/bombayjs-companion-app/quotesSpec.js
@@ -22,10 +22,6 @@ describe('JavaScript addition operator', function () {
     const conn = bombay.server.connect();
     console.log('conn = ' + conn);
     conn.then(function() {
-      return bombay.client.visit('http://www.cnn.com', 2000);
-    }).then(function() {
-      return bombay.client.visit('http://localhost:7777', 2000);
-    }).then(function () {
       return bombay.client.exists('#quote-details');
     }).then(function (results) {
       // expect(results).toBe(false);
